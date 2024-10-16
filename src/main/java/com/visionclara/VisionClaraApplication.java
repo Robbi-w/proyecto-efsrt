10236232590f2d7cab336ae320d06084da234f9d
Robbi Mendez <robbiwilian5@gmail.com>
2024-10-16 09:38:21 -0500

    Primer commit - Proyecto EFSRT

---
+++ src/main/java/com/visionclara/VisionClaraApplication.java
@@ -0,0 +1,13 @@
+package com.visionclara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionClaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionClaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visionclara/VisionClaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionclara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionClaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
